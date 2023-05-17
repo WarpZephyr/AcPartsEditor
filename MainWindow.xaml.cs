c600ed24e021085978c0eda33d96ff231f8e620b
natsudragneelthefiredragon <warpzephyr@gmail.com>
2023-05-17 18:03:34 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AcPartsEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
